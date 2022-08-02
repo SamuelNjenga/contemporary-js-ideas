005e38daeaa04db17140b59d1147d3edc00ab6ba
Samuel Njenga <snjenga704@gmail.com>
2022-08-02 22:37:44 +0300

    feat: add index_seven.js

---
+++ index_seven.js
@@ -0,0 +1,13 @@
+// Equality Operators
+
+const var_one = 44;
+const var_two = "44";
+
+console.log(var_one === var_two);
+console.log(var_one == var_two);
+
+const var_three = "1";
+const var_four = true;
+
+console.log(var_three === var_four);
+console.log(var_three == var_four);
