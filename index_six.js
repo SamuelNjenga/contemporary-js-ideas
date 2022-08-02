55b087630c062fdc5aec29af04d693a9aef74ecb
Samuel Njenga <snjenga704@gmail.com>
2022-08-02 22:18:11 +0300

    feat: add index_six.js

---
+++ index_six.js
@@ -0,0 +1,18 @@
+// use strict -> Indicates that the code should be executed in "strict mode"
+// It helps one write cleaner code and prevents one from using undeclared variables
+"use strict";
+
+// x = 3.14; // This will cause an error because x is not declared
+
+// function myFunction() {
+//   y = 3.14; // This will also cause an error because y is not declared
+// }
+// myFunction()
+
+
+//Deleting a variable or object is not allowed
+// let x = 3.14;
+// delete x;
+
+// The word eval cannot be used as a variable
+let eval = 3.14; 
