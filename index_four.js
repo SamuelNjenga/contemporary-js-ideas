cbc6835d1023203093944faccec5cc076bdd4cde
Samuel Njenga <snjenga704@gmail.com>
2022-08-02 14:45:38 +0300

    feat: add index_four.js

---
+++ index_four.js
@@ -0,0 +1,14 @@
+// DEFINING A FUNCTION
+
+// AS A FUNCTION EXPRESSION
+let foo_one = function () {
+  console.log("FUNCTION EXPRESSION");
+};
+
+// AS A FUNCTION STATEMENT
+function foo_two() {
+  console.log("FUNCTION STATEMENT");
+}
+
+foo_one();
+foo_two();
